ef4b7e3d0fc5164dc360bfb8ce03d39343597789
Benedek Fazekas <benedek.fazekas@gmail.com>
2015-02-15 19:07:13 +0000

    adds yasnippet conf for ruby

--- fb-ruby.el
+++ fb-ruby.el
@@ -1,6 +1,10 @@
 ;; inf ruby
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 
+;;yasnippet
+(add-hook 'ruby-mode-hook 'yas-minor-mode)
+(add-hook 'ruby-mode-hook 'yas-reload-all)
+
 ;; company
 (add-hook 'ruby-mode-hook 'company-mode)
 (eval-after-load 'company
