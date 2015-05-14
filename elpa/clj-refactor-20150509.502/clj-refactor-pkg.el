6f148a564008f21cbbb37683e7229bd4387f8f33
Benedek Fazekas <benedek.fazekas@gmail.com>
2015-05-14 12:19:50 +0100

    clj-refactor upgrade to snapshot

---
+++ elpa/clj-refactor-20150509.502/clj-refactor-pkg.el
@@ -0,0 +1,12 @@
+(define-package "clj-refactor" "20150509.502" "A collection of clojure refactoring functions"
+  '((s "1.8.0")
+    (dash "2.4.0")
+    (yasnippet "0.6.1")
+    (paredit "24")
+    (multiple-cursors "1.2.2")
+    (cider "0.8.1"))
+  :keywords
+  '("convenience"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
