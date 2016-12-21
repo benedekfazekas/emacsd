a5100e251c2c438034170f3435f799d5015af252
Benedek Fazekas <benedek.fazekas@gmail.com>
2016-12-21 13:22:25 +0000

    clj related upgrades

---
+++ elpa/clojure-mode-20161219.1500/clojure-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-mode" "20161219.1500" "Major mode for Clojure code" '((emacs "24.4")) :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "clojure" "clojurescript" "lisp"))
